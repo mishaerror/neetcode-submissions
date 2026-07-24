67987bf4070c36794ea33f29f7797420722c6440
mishaerror <milos.sto82@gmail.com>
2026-07-24 11:25:37 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    static final List<Character> OPENERS = List.of('(','{','[');
+    static final List<Character> CLOSERS = List.of(')','}',']');
+    static final Map<Character, Character> OPENER_FOR = Map.of(')','(','}','{',']','[');
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if(OPENERS.contains(c)) {
+                stack.push(c);
+            } else {
+                if(stack.empty() || !stack.peek().equals(OPENER_FOR.get(c))) {
+                    return false;
+                }
+                
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+}
